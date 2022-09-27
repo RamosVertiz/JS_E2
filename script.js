d6f534574d45679f926e4654054d9502be7482b7
Ezequiel Ramos Vertiz <ezequielramosvertiz@gmail.com>
2022-09-26 21:08:29 -0300

    E3

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-// Vamos a utilizar el mismo array de objetos "Pizzas🍕" del desafío general anterior. 
+// ¡Volvemos a trabajar con nuestro array de Pizzas🍕 !:
 
-// 👉 Crear un archivo HTML que contenga un h2, un h4, un input number y un botón. 
+// Deberán realizar el siguiente desafio: 
 
-// 👉 El desafío será, al tocar el botón, capturar el valor ingresado en el input.
-// 👉 Renderizar en el h2 el nombre y en el h4 el precio de la pizza cuyo id coincida con el numero ingresado en el input. 
+// 👉 A cada Pizza, agregarle una imagen. 
+// 👉 Crear un archivo HTML que contenga un card en donde se renderice el nombre, imagen, ingredientes y precio de una pizza (Estilizarlo con CSS 🎨). 
+// 👉 Debajo del card tiene que haber un input y un botón. 
 
-// 🚨 Si no coincide con ningún id, renderizar un mensaje de error. 
+// Deberemos colocar un numero en el input y, al apretar el botón, deberá renderizar en el card la pizza cuyo id coincida con el numero ingresado en el input.
 
+// 🚨 Si no coincide con ningún id, renderizar un mensaje de error.
+
+// ¿Cuál es el desafío final?
+// Deberán guardar en localStorage la última pizza buscada y renderizada, y al recargar la página será esa pizza la que se deberá mostrar en la página.
+
+
+// 🆙 Entregar el link de Github , en el cual debe estar linkeado el deploy del Vercel de su aplicación (mediante Github nosotros deberíamos poder ver el Vercel vinculado a su repositorio). 
 
 // declaro variables
 const input = document.getElementById("input");
@@ -14,75 +22,101 @@ const h2 = document.getElementById("nombrePizza");
 const h4 = document.getElementById("precioPizza");
 const btn = document.getElementById("boton");
 const p = document.getElementById("ingredientesPizza");
+const foto = document.getElementById("fotoPizza");
+
+
 
 class pizza {
-    constructor(id, nombre, ingredientes, precio){
+    constructor(id, nombre, ingredientes, precio, foto){
         this.id = id;
         this.nombre = nombre;
         this.ingredientes = ingredientes;
         this.precio = precio;
-        
+        this.foto = foto;
     }
 }
 const pizzas = [
     {
     id: 1,
     nombre: 'Muzzarella',
-    ingredientes: ['muzzarella', 'salsa de tomate', 'oregano'] , 
+    ingredientes: ['muzzarella', ' salsa de tomate', ' oregano.'] , 
     precio:  '$ 800',
+    foto: 'fotomuzza'
     },
     {
     id: 2,
     nombre: 'Fugazzeta',
-    ingredientes: ['muzzarella', 'cebolla', 'oregano'] , 
+    ingredientes: ['muzzarella', ' cebolla', ' oregano.'] , 
     precio: '$ 900',
+    foto: 'fotofuga'
     },
     {
     id: 3,
-    nombre: 'Jamon y morron',
-    ingredientes:  ['muzzarella', 'jamon', 'morron', 'oregano'] ,  
+    nombre: 'Jamon y morrones',
+    ingredientes:  ['muzzarella', ' jamon', ' morron', ' oregano.'] ,  
     precio: '$ 1000',
+    foto: 'fotojym'
     },
     {
     id: 4,
-    nombre: 'Rucula',
-    ingredientes:  ['muzzarella', 'rucula', 'parmesano'] ,  
+    nombre: 'Rúcula',
+    ingredientes:  ['muzzarella', ' rucula', ' tomates secos.'] ,  
     precio: '$ 1000',
+    foto: 'fotorucula'
     },
     {
     id: 5,
-    nombre: 'Napo',
-    ingredientes: ['muzzarella', 'tomate natural', 'ajo','parmesano'] ,  
+    nombre: 'Napolitana',
+    ingredientes: ['muzzarella', ' tomate natural', ' ajo',' parmesano.'] ,  
     precio: '$ 1000',
+    foto: 'fotonapo'
     }, 
     {
     id: 6,
     nombre: 'Canchera',
-    ingredientes: ['salsa de tomate', 'ajo'],
+    ingredientes: ['salsa de tomate', ' ajo.'],
     precio: '$ 550',
+    foto: 'fotocanchera'
     }   
 ]
+
+// const saveLocalStorage = (pizzas) => {
+//     localStorage.setItem('menu', JSON.stringify(pizzas))
+// };
+// saveLocalStorage(pizzas)
+
+// let menuPizzas = JSON.parse(localStorage.getItem('menu'));
+
 
 boton.addEventListener('click', button)
 
 function seleccion(eleccion) {
     const pizzaElegida = pizzas.filter((pizza) => pizza.id == eleccion)
+
     for (pizza of pizzaElegida) {
         nombrePizza.innerHTML = `${pizza.nombre}`;
         precioPizza.innerHTML = `${pizza.precio}`;
-        
-        
+        ingredientesPizza.innerHTML = `Ingredientes: ${pizza.ingredientes}`;
+        fotoPizza.innerHTML = `<img src=${pizza.foto}.jpg>`;
     }
 }
 
-function MensajeError() {
+function mensajeError() {
 
-    nombrePizza.innerHTML = 'Tenes que elegir un nro entre 1 y 6';
+    nombrePizza.innerHTML = 'El id ' + input.value + ' es invalido. Tenes que elegir un numero entre 1 y 6';
     precioPizza.innerHTML = '';
-
+    ingredientesPizza.innerHTML = '';
+    fotoPizza.innerHTML = '';
+    
 }
 
-
+function mensajeErrorVacio() {
+    nombrePizza.innerHTML = 'Tenes que ingresar un numero';
+    precioPizza.innerHTML = '';
+    ingredientesPizza.innerHTML = '';
+    fotoPizza.innerHTML = '';
+    
+}
 
 
 function button(e) {
@@ -90,10 +124,22 @@ function button(e) {
     const eleccion = input.value;
     if (eleccion <= pizzas.length && eleccion > 0) {
         seleccion(eleccion)
+        document.getElementById("cardPizza").className = "card"
+        document.getElementById("nombrePizza").className = "negro"
+        
+    }
+    else if (eleccion == ''){
+        mensajeErrorVacio()
+        document.getElementById("cardPizza").className = "card"
+        document.getElementById("nombrePizza").className = "rojo"
     }
     else {
-        MensajeError()
+        mensajeError()
+        document.getElementById("cardPizza").className = "card"
+        document.getElementById("nombrePizza").className = "rojo"
     }
     
-    input.value = ""
+    input.value = "";
+    
+    
 }
